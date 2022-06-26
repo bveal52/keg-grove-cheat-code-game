9a2bf865fffd2672052b89285ca6ace4c8300ca1
bveal <bveal52@gmail.com>
2022-06-26 16:55:54 -0400

    fixed canvas, player movement

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,8 +6,8 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 class Player {
 	constructor(){
@@ -44,23 +44,83 @@ class Player {
 
 
 	draw() {
-		// c.fillStyle = "red";
-		// c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+		c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+		
+	}
+
+	update() {
 		if(this.image)	{
-			c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-		}
+			this.draw()
+			this.position.x += this.velocity.x
+		 }
 	}
 }
 
+const player = new Player()
+const keys = {
+	a: {
+		pressed: false
+	}, 
+	d: {
+		pressed: false
+	},
+	space: {
+		pressed: false
+	}
 
+}
 
 function animate(){
 	requestAnimationFrame(animate)
-		c.fillStyle = 'black'
-		c.fillRect(0,0, this.width, this.height)
-	player.draw()
+	c.fillStyle = 'black'
+	c.fillRect(0,0, canvas.width, canvas.height)
+	player.update()
+
+	if(keys.a.pressed) {
+		player.velocity.x = -5
+	} else if (keys.d.pressed) {
+		player.velocity.x = 5
+	} else {
+		player.velocity.x = 0
+	}
+	
+	
 }
 
-const player = new Player()
-
 animate()
+
+
+addEventListener('keydown', ({key}) => {
+	switch(key) {
+		case 'a':
+			console.log("left")
+			keys.a.pressed = true
+			break;
+		case 'd':
+			console.log("right")
+			keys.d.pressed = true
+			break;
+		case ' ':
+			console.log("space")
+			keys.space.pressed = true
+			break;
+	}
+})
+
+addEventListener('keyup', ({key}) => {
+	switch(key) {
+		case 'a':
+			console.log("leftoff")
+			keys.a.pressed = false
+			break;
+		case 'd':
+			console.log("rightoff")
+			keys.d.pressed = false
+			break;
+		case ' ':
+			console.log("spaceoff")
+			keys.space.pressed = false
+			break;
+	}
+})
